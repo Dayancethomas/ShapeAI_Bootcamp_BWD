568f0e5b7f6607bf1321e33323e6a434e9618c47
Dayancethomas <86565229+Dayancethomas@users.noreply.github.com>
2021-06-27 23:30:25 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+       <h1>Javascript and React.js</h1>
+      <p>7 Days Long Free Basic Web Development React Js Bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
